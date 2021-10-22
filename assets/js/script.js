7df55b4d08228f8fe119cb07853e19b471ded031
Troy Allen <jake.allen@me.com>
2021-10-21 20:34:26 -0400

    add Dummy Fetch Data

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,3 +52,43 @@ function initStorage() {
 }
 
 initStorage()
+
+const fakeData = {
+    "symbol": "BTC",
+    "show_symbol": "BTC",
+    "name": "Bitcoin",
+    "rank": 1,
+    "price": "5524.7112165586",
+    "market_cap": "94433817003.39",
+    "total_volume_24h": "6378793658.5432",
+    "low_24h": "5324.2665427149",
+    "high_24h": "5561.0068476948",
+    "delta_1h": "0.81",
+    "delta_24h": "0.68",
+    "delta_7d": "-15.26",
+    "delta_30d": "-25.26",
+    "markets": [
+        {
+            "symbol": "EUR",
+            "volume_24h": "123707000",
+            "price": "5524.7112165586",
+            "exchanges": [
+                {
+                    "name": "Kraken",
+                    "volume_24h": "50623900",
+                    "price": "5520"
+                },
+                {
+                    "name": "Bitfinex",
+                    "volume_24h": "19314700",
+                    "price": "5512.6"
+                },
+                {...}
+            ]
+        },
+        {...},
+        {...}
+    ],
+    "last_updated_timestamp": 1528987416,
+    "remaining": 1133
+};
